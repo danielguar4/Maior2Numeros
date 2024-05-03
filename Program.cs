dae1922d94e589626149e466bfddfc33bc87e8e4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:11:24 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int First, Second;
+
+Console.Write("Digite o primeiro número...: ");
+First = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o Segundo Numero....: ");
+Second = Convert.ToInt32(Console.ReadLine());
+
+if (First > Second)
+    {
+        Console.WriteLine($"O maior número é {First}");
+    }
+else
+    {
+        Console.WriteLine($"O maior número é {Second}");
+    }
